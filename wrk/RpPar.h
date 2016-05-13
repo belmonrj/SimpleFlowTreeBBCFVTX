696806db6109c7f7d68dca044e4bc08936706f37
Ron Belmont <belmonrj@gmail.com>
2016-05-13 18:10:35 -0400

    Some comments

--- wrk/RpPar.h
+++ wrk/RpPar.h
@@ -1,15 +1,15 @@
 #ifndef RPPAR_H_
 #define RPPAR_H_
-static const int NDET = 8;
-static const int NHAR = 3;
-static const int NMUL = 1;
-static const int NZPS = 10;//
-static const int NORD = 12;//flattening order
-static const int NZED = 10;//matching
-static const int NMAT = 8;//all of these are for cnt matching
-static const int NPAR = 20;//for cnt matchint
-static const int NETA = 8;
-static const int NTRK = 50;
+static const int NDET = 8; // number of detectors (why 8?)
+static const int NHAR = 3; // number of harmonics (1, 2, 3)
+static const int NMUL = 1; // ???
+static const int NZPS = 10; // number of z bins for mixing???
+static const int NORD = 12; // flattening order ???
+static const int NZED = 10; // matching ???
+static const int NMAT = 8; // all of these are for cnt matching ???
+static const int NPAR = 20; // for cnt matching ???
+static const int NETA = 8; // ???
+static const int NTRK = 50; // ???
 
 const float phbeta = 29.9792458;
 const float mpion  = 0.139570;
