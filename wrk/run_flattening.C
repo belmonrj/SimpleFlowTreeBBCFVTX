09e4a0068c49e9105e78d642054e48cba3c97c11
Ron Belmont <belmonrj@gmail.com>
2016-05-20 00:36:26 -0400

    Added run flag to run_flattening

--- wrk/run_flattening.C
+++ wrk/run_flattening.C
@@ -1,12 +1,12 @@
-void run_flattening()
+void run_flattening(int run = 454936)
 {
 
   gROOT->ProcessLine(".L post_ana_bbc_fvtx.cpp++");
 
-  post_ana_bbc_fvtx(454811,1);
+  post_ana_bbc_fvtx(run,1);
   gROOT->Reset();
-  post_ana_bbc_fvtx(454811,2);
+  post_ana_bbc_fvtx(run,2);
   gROOT->Reset();
-  post_ana_bbc_fvtx(454811,3);
+  post_ana_bbc_fvtx(run,3);
 
 }
