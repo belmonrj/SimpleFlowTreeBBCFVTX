640a7b22a4ee556c1db3c6f5041e508c9059502e
Ron Belmont <belmonrj@gmail.com>
2016-05-24 13:26:49 -0400

    Plotting updates for draw_ep

--- wrk/MakePlots/draw_ep.C
+++ wrk/MakePlots/draw_ep.C
@@ -4,13 +4,22 @@ void draw_ep()
   TCanvas* c1 = new TCanvas("c1","");
 
   TFile* file = TFile::Open("input/EverythingIsCopasetic/hrp_455050.root");
+  //TFile* file = TFile::Open("input/hrp_455795.root");
 
   TH1D* psi2_BBC_before = (TH1D*)((TH2D*)file->Get("psi_bf_0_1_2"))->ProjectionY();
   TH1D* psi2_BBC_after = (TH1D*)((TH2D*)file->Get("psi_af_0_1_2"))->ProjectionY();
   psi2_BBC_before->Draw();
   psi2_BBC_before->GetXaxis()->SetRangeUser(-2.0,2.0);
+  psi2_BBC_before->GetXaxis()->SetTitle("#Psi_{2}");
   psi2_BBC_after->SetLineColor(kRed);
   psi2_BBC_after->Draw("same");
+  TLegend* leg;
+  leg = new TLegend(0.4,0.2,0.6,0.4);
+  leg->SetHeader("BBC");
+  leg->AddEntry(psi2_BBC_before,"Before","el");
+  leg->AddEntry(psi2_BBC_after,"After","el");
+  leg->SetTextSize(0.05);
+  leg->Draw();
   c1->Print("flattening_psi2_BBC_455050.png");
   c1->Print("flattening_psi2_BBC_455050.pdf");
 
@@ -18,8 +27,11 @@ void draw_ep()
   TH1D* psi2_FVTX_after = (TH1D*)((TH2D*)file->Get("psi_af_0_1_3"))->ProjectionY();
   psi2_FVTX_before->Draw();
   psi2_FVTX_before->GetXaxis()->SetRangeUser(-2.0,2.0);
+  psi2_FVTX_before->GetXaxis()->SetTitle("#Psi_{3}");
   psi2_FVTX_after->SetLineColor(kRed);
   psi2_FVTX_after->Draw("same");
+  leg->SetHeader("FVTX");
+  leg->Draw();
   c1->Print("flattening_psi2_FVTX_455050.png");
   c1->Print("flattening_psi2_FVTX_455050.pdf");
 
@@ -29,8 +41,11 @@ void draw_ep()
   TH1D* psi3_BBC_after = (TH1D*)((TH2D*)file->Get("psi_af_0_2_2"))->ProjectionY();
   psi3_BBC_before->Draw();
   psi3_BBC_before->GetXaxis()->SetRangeUser(-2.0,2.0);
+  psi3_BBC_before->GetXaxis()->SetTitle("#Psi_{3}");
   psi3_BBC_after->SetLineColor(kRed);
   psi3_BBC_after->Draw("same");
+  leg->SetHeader("BBC");
+  leg->Draw();
   c1->Print("flattening_psi3_BBC_455050.png");
   c1->Print("flattening_psi3_BBC_455050.pdf");
 
@@ -38,8 +53,11 @@ void draw_ep()
   TH1D* psi3_FVTX_after = (TH1D*)((TH2D*)file->Get("psi_af_0_2_3"))->ProjectionY();
   psi3_FVTX_before->Draw();
   psi3_FVTX_before->GetXaxis()->SetRangeUser(-2.0,2.0);
+  psi3_FVTX_before->GetXaxis()->SetTitle("#Psi_{3}");
   psi3_FVTX_after->SetLineColor(kRed);
   psi3_FVTX_after->Draw("same");
+  leg->SetHeader("FVTX");
+  leg->Draw();
   c1->Print("flattening_psi3_FVTX_455050.png");
   c1->Print("flattening_psi3_FVTX_455050.pdf");
 
