81112e93965026b90297b900c5d9c32087ad59cc
Ron Belmont <belmonrj@gmail.com>
2016-05-25 14:29:38 -0400

    Minor update to compile.sh

--- wrk/compile.sh
+++ wrk/compile.sh
@@ -3,3 +3,5 @@
 #g++ -std=c++11 -o flatten.exe flattening.cpp -lstdc++ `root-config --libs` -I$ROOTSYS/include
 g++ -o flattening.exe flattening.cpp -m32 -lstdc++ `root-config --libs` -I$ROOTSYS/include
 
+g++ -o rasplushchit.exe rasplushchit.cpp -m32 -lstdc++ `root-config --libs` -I$ROOTSYS/include
+
