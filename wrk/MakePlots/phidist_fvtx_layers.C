5a6e065c16679606f3170678a14ff379199eb787
Ron Belmont <belmonrj@gmail.com>
2016-06-30 15:47:50 -0400

    Added histogram titles and changed output fig names to include directory

--- wrk/MakePlots/phidist_fvtx_layers.C
+++ wrk/MakePlots/phidist_fvtx_layers.C
@@ -14,6 +14,8 @@ void phidist_fvtx_layers()
 void doe(int energy)
 {
 
+  gStyle->SetOptTitle(1);
+
   TCanvas* c1 = new TCanvas("c1","");
 
   TFile* file = TFile::Open(Form("input/combined_%d.root",energy));
@@ -26,38 +28,43 @@ void doe(int energy)
 
   th1d_south_layerA->Draw();
   th1d_south_layerA->SetMinimum(0);
+  th1d_south_layerA->SetTitle(Form("FVTX South, all layers, %d GeV",energy));
   th1d_south_layerA->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_south_layerA->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxs_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxs_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs_energy%d.pdf",energy));
 
   th1d_south_layer0->Draw();
   th1d_south_layer0->SetMinimum(0);
+  th1d_south_layer0->SetTitle(Form("FVTX South, layer 0, %d GeV",energy));
   th1d_south_layer0->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_south_layer0->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxs0_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxs0_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs0_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs0_energy%d.pdf",energy));
 
   th1d_south_layer1->Draw();
   th1d_south_layer1->SetMinimum(0);
+  th1d_south_layer1->SetTitle(Form("FVTX South, layer 1, %d GeV",energy));
   th1d_south_layer1->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_south_layer1->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxs1_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxs1_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs1_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs1_energy%d.pdf",energy));
 
   th1d_south_layer2->Draw();
   th1d_south_layer2->SetMinimum(0);
+  th1d_south_layer2->SetTitle(Form("FVTX South, layer 2, %d GeV",energy));
   th1d_south_layer2->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_south_layer2->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxs2_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxs2_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs2_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs2_energy%d.pdf",energy));
 
   th1d_south_layer3->Draw();
   th1d_south_layer3->SetMinimum(0);
+  th1d_south_layer3->SetTitle(Form("FVTX South, layer 3, %d GeV",energy));
   th1d_south_layer3->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_south_layer3->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxs3_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxs3_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs3_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxs3_energy%d.pdf",energy));
 
   // ---
 
@@ -69,38 +76,43 @@ void doe(int energy)
 
   th1d_north_layerA->Draw();
   th1d_north_layerA->SetMinimum(0);
+  th1d_north_layerA->SetTitle(Form("FVTX North, all layers, %d GeV",energy));
   th1d_north_layerA->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_north_layerA->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxn_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxn_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn_energy%d.pdf",energy));
 
   th1d_north_layer0->Draw();
   th1d_north_layer0->SetMinimum(0);
+  th1d_north_layer0->SetTitle(Form("FVTX North, layer 0, %d GeV",energy));
   th1d_north_layer0->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_north_layer0->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxn0_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxn0_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn0_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn0_energy%d.pdf",energy));
 
   th1d_north_layer1->Draw();
   th1d_north_layer1->SetMinimum(0);
+  th1d_north_layer1->SetTitle(Form("FVTX North, layer 1, %d GeV",energy));
   th1d_north_layer1->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_north_layer1->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxn1_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxn1_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn1_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn1_energy%d.pdf",energy));
 
   th1d_north_layer2->Draw();
   th1d_north_layer2->SetMinimum(0);
+  th1d_north_layer2->SetTitle(Form("FVTX North, layer 2, %d GeV",energy));
   th1d_north_layer2->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_north_layer2->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxn2_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxn2_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn2_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn2_energy%d.pdf",energy));
 
   th1d_north_layer3->Draw();
   th1d_north_layer3->SetMinimum(0);
+  th1d_north_layer3->SetTitle(Form("FVTX North, layer 3, %d GeV",energy));
   th1d_north_layer3->GetXaxis()->SetTitle("cluster #phi (rad)");
   th1d_north_layer3->GetYaxis()->SetTitle("counts");
-  c1->Print(Form("cluster_phidist_fvtxn3_energy%d.png",energy));
-  c1->Print(Form("cluster_phidist_fvtxn3_energy%d.pdf",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn3_energy%d.png",energy));
+  c1->Print(Form("FigsOther/cluster_phidist_fvtxn3_energy%d.pdf",energy));
 
 
 
