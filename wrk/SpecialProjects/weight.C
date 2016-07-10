503f9dec5f0b81cb3957ac54d549959e046de034
Ron Belmont <belmonrj@gmail.com>
2016-07-09 23:18:15 -0400

    Modified weight to loop over run numbers

--- wrk/SpecialProjects/weight.C
+++ wrk/SpecialProjects/weight.C
@@ -1,15 +1,43 @@
+void runweight(int);
+
+
 void weight()
+{
+
+  // runweight(456652);
+
+  // return;
+
+  int run;
+  ifstream fin;
+
+  fin.open("list_20.short");
+  while ( fin >> run ) runweight(run);
+  fin.close();
+
+  fin.open("list_39.short");
+  while ( fin >> run ) runweight(run);
+  fin.close();
+
+}
+
+
+void runweight(int run)
 {
 
   TCanvas* c1 = new TCanvas("c1","");
 
-  TFile* file = TFile::Open(Form("RootFiles/sum20_better.root"));
+  TFile* file = TFile::Open(Form("RootFiles/svrb_run%d_pass0.root",run));
+  if ( !file )
+    {
+      cout << "WARNING: file does not exist for run " << run << endl;
+      return;
+    }
   TH1D* th1d_fvtxs_clus_phi_IR = (TH1D*)file->Get("th1d_fvtxs_clus_phi_IR");
   TH1D* th1d_fvtxs0_clus_phi_IR = (TH1D*)file->Get("th1d_fvtxs0_clus_phi_IR");
   TH1D* th1d_fvtxs1_clus_phi_IR = (TH1D*)file->Get("th1d_fvtxs1_clus_phi_IR");
   TH1D* th1d_fvtxs2_clus_phi_IR = (TH1D*)file->Get("th1d_fvtxs2_clus_phi_IR");
   TH1D* th1d_fvtxs3_clus_phi_IR = (TH1D*)file->Get("th1d_fvtxs3_clus_phi_IR");
-
 
   const int nbins = 50;
   if ( th1d_fvtxs_clus_phi_IR->GetNbinsX() != nbins )
@@ -34,7 +62,6 @@ void weight()
   double pweight1[nbins];
   double pweight2[nbins];
   double pweight3[nbins];
-  ofstream fout((const char*)Form("weight.dat"));
   for ( int i = 0; i < nbins; ++i )
     {
       double phi = th1d_fvtxs_clus_phi_IR->GetBinCenter(i+1);
@@ -54,14 +81,7 @@ void weight()
       pweight1[i] = weight1;
       pweight2[i] = weight2;
       pweight3[i] = weight3;
-      fout << phi << " "
-           << weight << " "
-           << weight0 << " "
-           << weight1 << " "
-           << weight2 << " "
-           << weight3 << endl;
     }
-  fout.close();
 
   TGraph* tg_weight = new TGraph(nbins,pphi,pweight);
   tg_weight->SetMarkerStyle(kFullCircle);
@@ -69,7 +89,8 @@ void weight()
   tg_weight->SetMinimum(0.0);
   tg_weight->SetMaximum(4.0);
   tg_weight->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("weight.png"));
+  c1->Print(Form("FigsWeight/weight_run%d.png",run));
+  c1->Print(Form("FigsWeight/weight_run%d.pdf",run));
 
   TGraph* tg_weight0 = new TGraph(nbins,pphi,pweight0);
   tg_weight0->SetMarkerStyle(kFullCircle);
@@ -77,7 +98,8 @@ void weight()
   tg_weight0->SetMinimum(0.0);
   tg_weight0->SetMaximum(4.0);
   tg_weight0->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("weight0.png"));
+  c1->Print(Form("FigsWeight/weight0_run%d.png",run));
+  c1->Print(Form("FigsWeight/weight0_run%d.pdf",run));
 
   TGraph* tg_weight1 = new TGraph(nbins,pphi,pweight1);
   tg_weight1->SetMarkerStyle(kFullCircle);
@@ -85,7 +107,8 @@ void weight()
   tg_weight1->SetMinimum(0.0);
   tg_weight1->SetMaximum(4.0);
   tg_weight1->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("weight1.png"));
+  c1->Print(Form("FigsWeight/weight1_run%d.png",run));
+  c1->Print(Form("FigsWeight/weight1_run%d.pdf",run));
 
   TGraph* tg_weight2 = new TGraph(nbins,pphi,pweight2);
   tg_weight2->SetMarkerStyle(kFullCircle);
@@ -93,7 +116,8 @@ void weight()
   tg_weight2->SetMinimum(0.0);
   tg_weight2->SetMaximum(4.0);
   tg_weight2->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("weight2.png"));
+  c1->Print(Form("FigsWeight/weight2_run%d.png",run));
+  c1->Print(Form("FigsWeight/weight2_run%d.pdf",run));
 
   TGraph* tg_weight3 = new TGraph(nbins,pphi,pweight3);
   tg_weight3->SetMarkerStyle(kFullCircle);
@@ -101,9 +125,34 @@ void weight()
   tg_weight3->SetMinimum(0.0);
   tg_weight3->SetMaximum(4.0);
   tg_weight3->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("weight3.png"));
+  c1->Print(Form("FigsWeight/weight3_run%d.png",run));
+  c1->Print(Form("FigsWeight/weight3_run%d.pdf",run));
 
-  fout.open((const char*)Form("fweight.dat"));
+  // ---
+
+  TFile* fout = TFile::Open(Form("WeightFiles/weight_run%d.root",run),"recreate");
+  TH1D* th1d_weight_fvtxs = new TH1D("th1d_weight_fvtxs","",nbins,-TMath::Pi(),TMath::Pi());
+  TH1D* th1d_weight_fvtxs0 = new TH1D("th1d_weight_fvtxs0","",nbins,-TMath::Pi(),TMath::Pi());
+  TH1D* th1d_weight_fvtxs1 = new TH1D("th1d_weight_fvtxs1","",nbins,-TMath::Pi(),TMath::Pi());
+  TH1D* th1d_weight_fvtxs2 = new TH1D("th1d_weight_fvtxs2","",nbins,-TMath::Pi(),TMath::Pi());
+  TH1D* th1d_weight_fvtxs3 = new TH1D("th1d_weight_fvtxs3","",nbins,-TMath::Pi(),TMath::Pi());
+
+  for ( int i = 0; i < nbins; ++i )
+    {
+      th1d_weight_fvtxs->SetBinContent(i+1,pweight[i]);
+      th1d_weight_fvtxs0->SetBinContent(i+1,pweight0[i]);
+      th1d_weight_fvtxs1->SetBinContent(i+1,pweight1[i]);
+      th1d_weight_fvtxs2->SetBinContent(i+1,pweight2[i]);
+      th1d_weight_fvtxs3->SetBinContent(i+1,pweight3[i]);
+    }
+
+  cout << "Attempting to write and close " << (const char*)Form("WeightFiles/weight_run%d.root",run) << endl;
+
+  fout->Write();
+  fout->Close();
+
+  // ---
+
   for ( int i = 0; i < nbins; ++i )
     {
       double phi = th1d_fvtxs_clus_phi_IR->GetBinCenter(i+1);
@@ -123,14 +172,7 @@ void weight()
       pweight1[i] = weight1;
       pweight2[i] = weight2;
       pweight3[i] = weight3;
-      fout << phi << " "
-           << weight << " "
-           << weight0 << " "
-           << weight1 << " "
-           << weight2 << " "
-           << weight3 << endl;
     }
-  fout.close();
 
   TGraph* tg_fweight = new TGraph(nbins,pphi,pweight);
   tg_fweight->SetMarkerStyle(kFullCircle);
@@ -138,7 +180,8 @@ void weight()
   tg_fweight->SetMinimum(0.0);
   tg_fweight->SetMaximum(2.0);
   tg_fweight->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("fweight.png"));
+  c1->Print(Form("FigsWeight/fweight_run%d.png",run));
+  c1->Print(Form("FigsWeight/fweight_run%d.pdf",run));
 
   TGraph* tg_fweight0 = new TGraph(nbins,pphi,pweight0);
   tg_fweight0->SetMarkerStyle(kFullCircle);
@@ -146,7 +189,8 @@ void weight()
   tg_fweight0->SetMinimum(0.0);
   tg_fweight0->SetMaximum(2.0);
   tg_fweight0->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("fweight0.png"));
+  c1->Print(Form("FigsWeight/fweight0_run%d.png",run));
+  c1->Print(Form("FigsWeight/fweight0_run%d.pdf",run));
 
   TGraph* tg_fweight1 = new TGraph(nbins,pphi,pweight1);
   tg_fweight1->SetMarkerStyle(kFullCircle);
@@ -154,7 +198,8 @@ void weight()
   tg_fweight1->SetMinimum(0.0);
   tg_fweight1->SetMaximum(2.0);
   tg_fweight1->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("fweight1.png"));
+  c1->Print(Form("FigsWeight/fweight1_run%d.png",run));
+  c1->Print(Form("FigsWeight/fweight1_run%d.pdf",run));
 
   TGraph* tg_fweight2 = new TGraph(nbins,pphi,pweight2);
   tg_fweight2->SetMarkerStyle(kFullCircle);
@@ -162,7 +207,8 @@ void weight()
   tg_fweight2->SetMinimum(0.0);
   tg_fweight2->SetMaximum(2.0);
   tg_fweight2->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("fweight2.png"));
+  c1->Print(Form("FigsWeight/fweight2_run%d.png",run));
+  c1->Print(Form("FigsWeight/fweight2_run%d.pdf",run));
 
   TGraph* tg_fweight3 = new TGraph(nbins,pphi,pweight3);
   tg_fweight3->SetMarkerStyle(kFullCircle);
@@ -170,6 +216,15 @@ void weight()
   tg_fweight3->SetMinimum(0.0);
   tg_fweight3->SetMaximum(2.0);
   tg_fweight3->GetXaxis()->SetLimits(-TMath::Pi(),TMath::Pi());
-  c1->Print(Form("fweight3.png"));
+  c1->Print(Form("FigsWeight/fweight3_run%d.png",run));
+  c1->Print(Form("FigsWeight/fweight3_run%d.pdf",run));
+
+  // ---
+  // ---
+  // ---
+
+  file->Close();
+
+  delete c1;
 
 }
