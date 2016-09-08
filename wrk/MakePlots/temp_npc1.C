f4319ffce9fb031e57260c5fdf089aafb91a2142
Ron Belmont <belmonrj@gmail.com>
2016-09-08 12:26:16 -0400

    Added correlation type selectivity

--- wrk/MakePlots/temp_npc1.C
+++ wrk/MakePlots/temp_npc1.C
@@ -1,6 +1,16 @@
 void temp_npc1()
 {
 
+  takename("bbcsfvtxs");
+  takename("bbcsfvtxn");
+  takename("fvtxsfvtxn");
+
+}
+
+
+void takename(const char* name)
+{
+
   TCanvas* c1 = new TCanvas();
 
   TFile* file_200 = TFile::Open("input/combined_200.root");
@@ -8,96 +18,103 @@ void temp_npc1()
   TFile* file_39 = TFile::Open("input/combined_39.root");
   TFile* file_20 = TFile::Open("input/combined_20.root");
 
-  TProfile* tp1f_fvtxsfvtxn_200 = (TProfile*)file_200->Get("npc1_os_fvtxsfvtxn_c22");
-  TProfile* tp1f_fvtxsfvtxn_62 = (TProfile*)file_62->Get("npc1_os_fvtxsfvtxn_c22");
-  TProfile* tp1f_fvtxsfvtxn_39 = (TProfile*)file_39->Get("npc1_os_fvtxsfvtxn_c22");
-  TProfile* tp1f_fvtxsfvtxn_20 = (TProfile*)file_20->Get("npc1_os_fvtxsfvtxn_c22");
+  TProfile* tp1f_200 = (TProfile*)file_200->Get(Form("npc1_os_%s_c22",name));
+  TProfile* tp1f_62 = (TProfile*)file_62->Get(Form("npc1_os_%s_c22",name));
+  TProfile* tp1f_39 = (TProfile*)file_39->Get(Form("npc1_os_%s_c22",name));
+  TProfile* tp1f_20 = (TProfile*)file_20->Get(Form("npc1_os_%s_c22",name));
 
-  tp1f_fvtxsfvtxn_200->SetName("tp1f_fvtxsfvtxn_200");
-  tp1f_fvtxsfvtxn_62->SetName("tp1f_fvtxsfvtxn_62");
-  tp1f_fvtxsfvtxn_39->SetName("tp1f_fvtxsfvtxn_39");
-  tp1f_fvtxsfvtxn_20->SetName("tp1f_fvtxsfvtxn_20");
+  tp1f_200->SetName("tp1f_200");
+  tp1f_62->SetName("tp1f_62");
+  tp1f_39->SetName("tp1f_39");
+  tp1f_20->SetName("tp1f_20");
 
-  TH1D* th1d_v2_fvtxsfvtxn_200 = sqrt(tp1f_fvtxsfvtxn_200);
-  TH1D* th1d_v2_fvtxsfvtxn_62 = sqrt(tp1f_fvtxsfvtxn_62);
-  TH1D* th1d_v2_fvtxsfvtxn_39 = sqrt(tp1f_fvtxsfvtxn_39);
-  TH1D* th1d_v2_fvtxsfvtxn_20 = sqrt(tp1f_fvtxsfvtxn_20);
+  TH1D* th1d_v2_200 = sqrt(tp1f_200);
+  TH1D* th1d_v2_62 = sqrt(tp1f_62);
+  TH1D* th1d_v2_39 = sqrt(tp1f_39);
+  TH1D* th1d_v2_20 = sqrt(tp1f_20);
 
-  th1d_v2_fvtxsfvtxn_200->Draw();
-  th1d_v2_fvtxsfvtxn_200->GetYaxis()->SetTitle("v_{2}{FVTXS-FVTXN}");
-  th1d_v2_fvtxsfvtxn_200->GetXaxis()->SetTitle("N_{PC1}");
-  c1->Print("run16dau_v2_npc1_200.png");
+  th1d_v2_200->Draw();
+  th1d_v2_200->GetYaxis()->SetTitle("v_{2}{2}");
+  th1d_v2_200->GetXaxis()->SetTitle("N_{PC1}");
+  c1->Print(Form("FigsMultiplicity/run16dau_v2_%s_npc1_200.png",name));
 
-  th1d_v2_fvtxsfvtxn_62->Draw();
-  th1d_v2_fvtxsfvtxn_62->GetYaxis()->SetTitle("v_{2}{FVTXS-FVTXN}");
-  th1d_v2_fvtxsfvtxn_62->GetXaxis()->SetTitle("N_{PC1}");
-  c1->Print("run16dau_v2_npc1_62.png");
+  th1d_v2_62->Draw();
+  th1d_v2_62->GetYaxis()->SetTitle("v_{2}{2}");
+  th1d_v2_62->GetXaxis()->SetTitle("N_{PC1}");
+  c1->Print(Form("FigsMultiplicity/run16dau_v2_%s_npc1_62.png",name));
 
-  th1d_v2_fvtxsfvtxn_39->Draw();
-  th1d_v2_fvtxsfvtxn_39->GetYaxis()->SetTitle("v_{2}{FVTXS-FVTXN}");
-  th1d_v2_fvtxsfvtxn_39->GetXaxis()->SetTitle("N_{PC1}");
-  c1->Print("run16dau_v2_npc1_39.png");
+  th1d_v2_39->Draw();
+  th1d_v2_39->GetYaxis()->SetTitle("v_{2}{2}");
+  th1d_v2_39->GetXaxis()->SetTitle("N_{PC1}");
+  c1->Print(Form("FigsMultiplicity/run16dau_v2_%s_npc1_39.png",name));
 
-  th1d_v2_fvtxsfvtxn_20->Draw();
-  th1d_v2_fvtxsfvtxn_20->GetYaxis()->SetTitle("v_{2}{FVTXS-FVTXN}");
-  th1d_v2_fvtxsfvtxn_20->GetXaxis()->SetTitle("N_{PC1}");
-  c1->Print("run16dau_v2_npc1_20.png");
+  th1d_v2_20->Draw();
+  th1d_v2_20->GetYaxis()->SetTitle("v_{2}{2}");
+  th1d_v2_20->GetXaxis()->SetTitle("N_{PC1}");
+  c1->Print(Form("FigsMultiplicity/run16dau_v2_%s_npc1_20.png",name));
 
   double npc1[61];
-  double v2_fvtxsfvtxn_200[61];
-  double v2_fvtxsfvtxn_62[61];
-  double v2_fvtxsfvtxn_39[61];
-  double v2_fvtxsfvtxn_20[61];
-  double ev2_fvtxsfvtxn_200[61];
-  double ev2_fvtxsfvtxn_62[61];
-  double ev2_fvtxsfvtxn_39[61];
-  double ev2_fvtxsfvtxn_20[61];
+  double v2_200[61];
+  double v2_62[61];
+  double v2_39[61];
+  double v2_20[61];
+  double ev2_200[61];
+  double ev2_62[61];
+  double ev2_39[61];
+  double ev2_20[61];
 
   for ( int i = 0; i < 61; ++i )
     {
-      npc1[i] = th1d_v2_fvtxsfvtxn_200->GetBinCenter(i+1);
-      v2_fvtxsfvtxn_200[i] = th1d_v2_fvtxsfvtxn_200->GetBinContent(i+1);
-      v2_fvtxsfvtxn_62[i] = th1d_v2_fvtxsfvtxn_62->GetBinContent(i+1);
-      v2_fvtxsfvtxn_39[i] = th1d_v2_fvtxsfvtxn_39->GetBinContent(i+1);
-      v2_fvtxsfvtxn_20[i] = th1d_v2_fvtxsfvtxn_20->GetBinContent(i+1);
-      ev2_fvtxsfvtxn_200[i] = th1d_v2_fvtxsfvtxn_200->GetBinError(i+1);
-      ev2_fvtxsfvtxn_62[i] = th1d_v2_fvtxsfvtxn_62->GetBinError(i+1);
-      ev2_fvtxsfvtxn_39[i] = th1d_v2_fvtxsfvtxn_39->GetBinError(i+1);
-      ev2_fvtxsfvtxn_20[i] = th1d_v2_fvtxsfvtxn_20->GetBinError(i+1);
-      if ( npc1[i] > 61 ) v2_fvtxsfvtxn_200[i] = -9;
-      if ( npc1[i] > 28 ) v2_fvtxsfvtxn_62[i] = -9;
-      if ( npc1[i] > 19 ) v2_fvtxsfvtxn_39[i] = -9;
-      if ( npc1[i] > 10 ) v2_fvtxsfvtxn_20[i] = -9;
+      npc1[i] = th1d_v2_200->GetBinCenter(i+1);
+      v2_200[i] = th1d_v2_200->GetBinContent(i+1);
+      v2_62[i] = th1d_v2_62->GetBinContent(i+1);
+      v2_39[i] = th1d_v2_39->GetBinContent(i+1);
+      v2_20[i] = th1d_v2_20->GetBinContent(i+1);
+      ev2_200[i] = th1d_v2_200->GetBinError(i+1);
+      ev2_62[i] = th1d_v2_62->GetBinError(i+1);
+      ev2_39[i] = th1d_v2_39->GetBinError(i+1);
+      ev2_20[i] = th1d_v2_20->GetBinError(i+1);
+      if ( npc1[i] > 61 ) v2_200[i] = -9;
+      if ( npc1[i] > 28 ) v2_62[i] = -9;
+      if ( npc1[i] > 19 ) v2_39[i] = -9;
+      if ( npc1[i] > 10 ) v2_20[i] = -9;
     }
 
   TH2D* hdummy = new TH2D("hdummy","",1,-0.5,60.5,1,0,0.08);
-  hdummy->GetYaxis()->SetTitle("v_{2}{FVTXS-FVTXN}");
+  hdummy->GetYaxis()->SetTitle("v_{2}{2}");
   hdummy->GetXaxis()->SetTitle("N_{PC1}");
-  TGraphErrors* tge_fvtxsfvtxn_200 = new TGraphErrors(61,npc1,v2_fvtxsfvtxn_200,0,ev2_fvtxsfvtxn_200);
-  TGraphErrors* tge_fvtxsfvtxn_62 = new TGraphErrors(61,npc1,v2_fvtxsfvtxn_62,0,ev2_fvtxsfvtxn_62);
-  TGraphErrors* tge_fvtxsfvtxn_39 = new TGraphErrors(61,npc1,v2_fvtxsfvtxn_39,0,ev2_fvtxsfvtxn_39);
-  TGraphErrors* tge_fvtxsfvtxn_20 = new TGraphErrors(61,npc1,v2_fvtxsfvtxn_20,0,ev2_fvtxsfvtxn_20);
-  tge_fvtxsfvtxn_200->SetMarkerStyle(kFullCircle);
-  tge_fvtxsfvtxn_62->SetMarkerStyle(kFullSquare);
-  tge_fvtxsfvtxn_39->SetMarkerStyle(kFullCross);
-  tge_fvtxsfvtxn_20->SetMarkerStyle(kFullDiamond);
-  tge_fvtxsfvtxn_200->SetMarkerColor(kBlack);
-  tge_fvtxsfvtxn_62->SetMarkerColor(kBlue);
-  tge_fvtxsfvtxn_39->SetMarkerColor(kRed);
-  tge_fvtxsfvtxn_20->SetMarkerColor(kGreen+2);
+  TGraphErrors* tge_200 = new TGraphErrors(61,npc1,v2_200,0,ev2_200);
+  TGraphErrors* tge_62 = new TGraphErrors(61,npc1,v2_62,0,ev2_62);
+  TGraphErrors* tge_39 = new TGraphErrors(61,npc1,v2_39,0,ev2_39);
+  TGraphErrors* tge_20 = new TGraphErrors(61,npc1,v2_20,0,ev2_20);
+  tge_200->SetMarkerStyle(kFullCircle);
+  tge_62->SetMarkerStyle(kFullSquare);
+  tge_39->SetMarkerStyle(kFullCross);
+  tge_20->SetMarkerStyle(kFullDiamond);
+  tge_200->SetMarkerColor(kBlack);
+  tge_62->SetMarkerColor(kBlue);
+  tge_39->SetMarkerColor(kRed);
+  tge_20->SetMarkerColor(kGreen+2);
   hdummy->Draw();
-  tge_fvtxsfvtxn_200->Draw("p");
-  tge_fvtxsfvtxn_62->Draw("p");
-  tge_fvtxsfvtxn_39->Draw("p");
-  tge_fvtxsfvtxn_20->Draw("p");
-  c1->Print("run16dau_v2_npc1_all.png");
+  tge_200->Draw("p");
+  tge_62->Draw("p");
+  tge_39->Draw("p");
+  tge_20->Draw("p");
+  TLegend* leg = new TLegend(0.68,0.68,0.88,0.88);
+  leg->AddEntry(tge_200,"200 GeV","p");
+  leg->AddEntry(tge_62,"62.4 GeV","p");
+  leg->AddEntry(tge_39,"39 GeV","p");
+  leg->AddEntry(tge_20,"19.6 GeV","p");
+  leg->SetTextSize(0.045);
+  leg->SetFillStyle(0);
+  leg->Draw();
+  c1->Print(Form("FigsMultiplicity/run16dau_v2_%s_npc1_all.png",name));
 
 }
 
 TH1D* sqrt(TProfile* tp1f)
 {
   TH1D* th1d = tp1f->ProjectionX(Form("%s_px",tp1f->GetName()));
-  TH1D* th1d = tp1f->ProjectionX();
   return sqrt(th1d);
 }
 
