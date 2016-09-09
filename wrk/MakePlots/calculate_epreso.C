0dc4fc45b1025ae3ca16ee2422b1e6a86db43e56
Ron Belmont <belmonrj@gmail.com>
2016-09-09 13:59:01 -0400

    Change to print out latex table

--- wrk/MakePlots/calculate_epreso.C
+++ wrk/MakePlots/calculate_epreso.C
@@ -3,13 +3,15 @@ void doenergy(int, int);
 void calculate_epreso()
 {
 
-  for ( int i = 2; i < 4; ++i )
-    {
-      doenergy(200,i);
-      doenergy(62,i);
-      doenergy(39,i);
-      doenergy(20,i);
-    }
+  doenergy(200,2);
+  doenergy(62,2);
+  doenergy(39,2);
+  doenergy(20,2);
+
+  doenergy(200,3);
+  doenergy(62,3);
+  doenergy(39,3);
+  doenergy(20,3);
 
 }
 
@@ -30,17 +32,17 @@ void doenergy(int energy, int harmonic)
   float float_bbc_fvtx = tp1f_bbc_fvtx->GetBinContent(1);
   float float_fvtxN_fvtx = tp1f_fvtxN_fvtx->GetBinContent(1);
 
-  cout << "bbc-fvtxn " << float_bbc_fvtxN << endl;
-  cout << "bbc-fvtxs " << float_bbc_fvtx << endl;
-  cout << "fvtxn-fvtxs " << float_fvtxN_fvtx << endl;
+  // cout << "bbc-fvtxn " << float_bbc_fvtxN << endl;
+  // cout << "bbc-fvtxs " << float_bbc_fvtx << endl;
+  // cout << "fvtxn-fvtxs " << float_fvtxN_fvtx << endl;
 
   float reso_bbc_fn = sqrt((float_bbc_fvtxN*float_bbc_fvtx)/float_fvtxN_fvtx); // BNBS/NS
   float reso_fvtx_fn = sqrt((float_fvtxN_fvtx*float_bbc_fvtx)/float_bbc_fvtxN); // NSBS/BN
   float reso_fvtxN_fn = sqrt((float_fvtxN_fvtx*float_bbc_fvtxN)/float_bbc_fvtx); // NSBN/BS
 
-  cout << "bbc resolution is " << reso_bbc_fn << endl;
-  cout << "fvtx resolution is " << reso_fvtx_fn << endl;
-  cout << "fvtxN resolution is " << reso_fvtxN_fn << endl;
+  // cout << "bbc resolution is " << reso_bbc_fn << endl;
+  // cout << "fvtx resolution is " << reso_fvtx_fn << endl;
+  // cout << "fvtxN resolution is " << reso_fvtxN_fn << endl;
 
   // ---
 
@@ -52,27 +54,31 @@ void doenergy(int energy, int harmonic)
   float float_cnt_fvtx = tp1f_cnt_fvtx->GetBinContent(1);
   float float_cnt_fvtxN = tp1f_cnt_fvtxN->GetBinContent(1);
 
-  cout << "bbc-cnt " << float_bbc_cnt << endl;
-  cout << "cnt-fvtxs " << float_cnt_fvtx << endl;
-  cout << "cbt-fvtxn " << float_cnt_fvtxN << endl;
+  // cout << "bbc-cnt " << float_bbc_cnt << endl;
+  // cout << "cnt-fvtxs " << float_cnt_fvtx << endl;
+  // cout << "cbt-fvtxn " << float_cnt_fvtxN << endl;
 
-  if ( float_bbc_cnt < 0 ) { cout << "YOU'RE GONNA DIE (bbcs-cnt)" << endl; }
-  if ( float_cnt_fvtx < 0 ) { cout << "YOU'RE GONNA DIE (cnt-fvtxs)" << endl; }
-  if ( float_cnt_fvtxN < 0 ) { cout << "YOU'RE GONNA DIE (cnt-fvtxn)" << endl; }
+  if ( float_bbc_cnt < 0 ) { /*float_bbc_cnt *= -1;*/ cout << "YOU'RE GONNA DIE (bbcs-cnt)" << endl; }
+  if ( float_bbc_fvtx < 0 ) { /*float_bbc_fvtx *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxs)" << endl; }
+  if ( float_cnt_fvtx < 0 ) { /*float_cnt_fvtx *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxs)" << endl; }
+  if ( float_cnt_fvtxN < 0 ) { /*float_cnt_fvtxN *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxn)" << endl; }
 
   float reso_bbc = sqrt((float_bbc_cnt*float_bbc_fvtx)/float_cnt_fvtx); // BCBS/CS
   float reso_fvtx = sqrt((float_cnt_fvtx*float_bbc_fvtx)/float_bbc_cnt); // CSBS/BC
   float reso_fvtxN = sqrt((float_cnt_fvtxN*float_bbc_fvtxN)/float_bbc_cnt); // CNBN/BC
 
-  cout << "bbc resolution is " << reso_bbc << endl;
-  cout << "fvtx resolution is " << reso_fvtx << endl;
-  cout << "fvtxN resolution is " << reso_fvtxN << endl;
+  //  cout << "bbc resolution is " << reso_bbc << endl;
+  //  cout << "fvtx resolution is " << reso_fvtx << endl;
+  //  cout << "fvtxN resolution is " << reso_fvtxN << endl;
 
   // ---
 
   float reso_fvtxN_xb = sqrt ( ( float_fvtxN_fvtx * float_cnt_fvtxN ) / float_cnt_fvtx ) ; // NSNC/SC
 
-  cout << "fvtxN resolution is " << reso_fvtxN_xb << endl;
+  // cout << "fvtxN resolution is " << reso_fvtxN_xb << endl;
+
+  cout << energy << " GeV & " << reso_bbc << " & " << reso_fvtx << " & " << float_bbc_fvtx << " & " << float_bbc_cnt << " & " << float_cnt_fvtx << " \\\\ " << endl;
+  cout << energy << " GeV & " << reso_bbc_fn << " & " << reso_fvtx_fn << " & " << reso_fvtxN_fn << " & " << float_bbc_fvtx << " & " << float_bbc_fvtxN << " & " << float_fvtxN_fvtx << " \\\\ " << endl;
 
 }
 
