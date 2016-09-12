47cb942a7c77859026a06ac141added61415b60c
Ron Belmont <belmonrj@gmail.com>
2016-09-12 12:02:52 -0400

    Changed variable names and added some stuff for printing to screen

--- wrk/MakePlots/calculate_epreso.C
+++ wrk/MakePlots/calculate_epreso.C
@@ -23,62 +23,57 @@ void doenergy(int energy, int harmonic)
   TFile* file = TFile::Open(Form("input/combined_%d.root",energy));
 
   // ---
-
-  TProfile* tp1f_bbc_fvtxN = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_FVTXN",harmonic));
-  TProfile* tp1f_bbc_fvtx = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_FVTX",harmonic));
-  TProfile* tp1f_fvtxN_fvtx = (TProfile*)file->Get(Form("tp1f_reso%d_FVTXS_FVTXN",harmonic));
-
-  float float_bbc_fvtxN = tp1f_bbc_fvtxN->GetBinContent(1);
-  float float_bbc_fvtx = tp1f_bbc_fvtx->GetBinContent(1);
-  float float_fvtxN_fvtx = tp1f_fvtxN_fvtx->GetBinContent(1);
-
-  // cout << "bbc-fvtxn " << float_bbc_fvtxN << endl;
-  // cout << "bbc-fvtxs " << float_bbc_fvtx << endl;
-  // cout << "fvtxn-fvtxs " << float_fvtxN_fvtx << endl;
-
-  float reso_bbc_fn = sqrt((float_bbc_fvtxN*float_bbc_fvtx)/float_fvtxN_fvtx); // BNBS/NS
-  float reso_fvtx_fn = sqrt((float_fvtxN_fvtx*float_bbc_fvtx)/float_bbc_fvtxN); // NSBS/BN
-  float reso_fvtxN_fn = sqrt((float_fvtxN_fvtx*float_bbc_fvtxN)/float_bbc_fvtx); // NSBN/BS
-
-  // cout << "bbc resolution is " << reso_bbc_fn << endl;
-  // cout << "fvtx resolution is " << reso_fvtx_fn << endl;
-  // cout << "fvtxN resolution is " << reso_fvtxN_fn << endl;
-
+  // ---
   // ---
 
-  TProfile* tp1f_bbc_cnt = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_CNT",harmonic));
-  TProfile* tp1f_cnt_fvtx = (TProfile*)file->Get(Form("tp1f_reso%d_CNT_FVTX",harmonic));
-  TProfile* tp1f_cnt_fvtxN = (TProfile*)file->Get(Form("tp1f_reso%d_CNT_FVTXN",harmonic));
+  TProfile* tp1f_BBCS_FVTXN = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_FVTXN",harmonic));
+  TProfile* tp1f_BBCS_FVTXS = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_FVTX",harmonic));
+  TProfile* tp1f_FVTXN_FVTXS = (TProfile*)file->Get(Form("tp1f_reso%d_FVTXS_FVTXN",harmonic));
+  // ---
+  float float_BBCS_FVTXN = tp1f_BBCS_FVTXN->GetBinContent(1);
+  float float_BBCS_FVTXS = tp1f_BBCS_FVTXS->GetBinContent(1);
+  float float_FVTXN_FVTXS = tp1f_FVTXN_FVTXS->GetBinContent(1);
+  float efloat_BBCS_FVTXN = tp1f_BBCS_FVTXN->GetBinError(1);
+  float efloat_BBCS_FVTXS = tp1f_BBCS_FVTXS->GetBinError(1);
+  float efloat_FVTXN_FVTXS = tp1f_FVTXN_FVTXS->GetBinError(1);
+  // ---
+  float reso_BBCS_fn = sqrt((float_BBCS_FVTXN*float_BBCS_FVTXS)/float_FVTXN_FVTXS); // BNBS/NS
+  float reso_FVTXS_fn = sqrt((float_FVTXN_FVTXS*float_BBCS_FVTXS)/float_BBCS_FVTXN); // NSBS/BN
+  float reso_FVTXN_fn = sqrt((float_FVTXN_FVTXS*float_BBCS_FVTXN)/float_BBCS_FVTXS); // NSBN/BS
 
-  float float_bbc_cnt = tp1f_bbc_cnt->GetBinContent(1);
-  float float_cnt_fvtx = tp1f_cnt_fvtx->GetBinContent(1);
-  float float_cnt_fvtxN = tp1f_cnt_fvtxN->GetBinContent(1);
-
-  // cout << "bbc-cnt " << float_bbc_cnt << endl;
-  // cout << "cnt-fvtxs " << float_cnt_fvtx << endl;
-  // cout << "cbt-fvtxn " << float_cnt_fvtxN << endl;
-
-  if ( float_bbc_cnt < 0 ) { /*float_bbc_cnt *= -1;*/ cout << "YOU'RE GONNA DIE (bbcs-cnt)" << endl; }
-  if ( float_bbc_fvtx < 0 ) { /*float_bbc_fvtx *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxs)" << endl; }
-  if ( float_cnt_fvtx < 0 ) { /*float_cnt_fvtx *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxs)" << endl; }
-  if ( float_cnt_fvtxN < 0 ) { /*float_cnt_fvtxN *= -1;*/ cout << "YOU'RE GONNA DIE (cnt-fvtxn)" << endl; }
-
-  float reso_bbc = sqrt((float_bbc_cnt*float_bbc_fvtx)/float_cnt_fvtx); // BCBS/CS
-  float reso_fvtx = sqrt((float_cnt_fvtx*float_bbc_fvtx)/float_bbc_cnt); // CSBS/BC
-  float reso_fvtxN = sqrt((float_cnt_fvtxN*float_bbc_fvtxN)/float_bbc_cnt); // CNBN/BC
-
-  //  cout << "bbc resolution is " << reso_bbc << endl;
-  //  cout << "fvtx resolution is " << reso_fvtx << endl;
-  //  cout << "fvtxN resolution is " << reso_fvtxN << endl;
+  TProfile* tp1f_BBCS_CNT = (TProfile*)file->Get(Form("tp1f_reso%d_BBC_CNT",harmonic));
+  TProfile* tp1f_CNT_FVTXS = (TProfile*)file->Get(Form("tp1f_reso%d_CNT_FVTX",harmonic));
+  TProfile* tp1f_CNT_FVTXN = (TProfile*)file->Get(Form("tp1f_reso%d_CNT_FVTXN",harmonic));
+  // ---
+  float float_BBCS_CNT = tp1f_BBCS_CNT->GetBinContent(1);
+  float float_CNT_FVTXS = tp1f_CNT_FVTXS->GetBinContent(1);
+  float float_CNT_FVTXN = tp1f_CNT_FVTXN->GetBinContent(1);
+  float efloat_BBCS_CNT = tp1f_BBCS_CNT->GetBinError(1);
+  float efloat_CNT_FVTXS = tp1f_CNT_FVTXS->GetBinError(1);
+  float efloat_CNT_FVTXN = tp1f_CNT_FVTXN->GetBinError(1);
+  // ---
+  float reso_BBCS = sqrt((float_BBCS_CNT*float_BBCS_FVTXS)/float_CNT_FVTXS); // BCBS/CS
+  float reso_FVTXS = sqrt((float_CNT_FVTXS*float_BBCS_FVTXS)/float_BBCS_CNT); // CSBS/BC
+  float reso_FVTXN = sqrt((float_CNT_FVTXN*float_BBCS_FVTXN)/float_BBCS_CNT); // CNBN/BC
+  float reso_FVTXN_xb = sqrt ( ( float_FVTXN_FVTXS * float_CNT_FVTXN ) / float_CNT_FVTXS ) ; // NSNC/SC
 
   // ---
+  float ereso_BBCS = sqrt( ( efloat_BBCS_CNT*efloat_BBCS_CNT/4*float_BBCS_CNT )
+                           + ( efloat_BBCS_FVTXS*efloat_BBCS_FVTXS/4*float_BBCS_FVTXS )
+                           + ( efloat_CNT_FVTXS*efloat_CNT_FVTXS/4*pow(float_CNT_FVTXS,3) ) );
 
-  float reso_fvtxN_xb = sqrt ( ( float_fvtxN_fvtx * float_cnt_fvtxN ) / float_cnt_fvtx ) ; // NSNC/SC
+  TString data1 = Form("%d GeV & %.2e & %.2e & %.2e \$\\pm\$ %.2e & %.2e \$\\pm\$ %.2e & %.2e \$\\pm\$ %.2e \\\\",
+                       energy,reso_BBCS,reso_FVTXS,
+                       float_BBCS_FVTXS,efloat_BBCS_FVTXS,
+                       float_BBCS_CNT,efloat_BBCS_CNT,
+                       float_CNT_FVTXS,efloat_CNT_FVTXS);
 
-  // cout << "fvtxN resolution is " << reso_fvtxN_xb << endl;
+  cout << data1.Data() << endl;
 
-  cout << energy << " GeV & " << reso_bbc << " & " << reso_fvtx << " & " << float_bbc_fvtx << " & " << float_bbc_cnt << " & " << float_cnt_fvtx << " \\\\ " << endl;
-  cout << energy << " GeV & " << reso_bbc_fn << " & " << reso_fvtx_fn << " & " << reso_fvtxN_fn << " & " << float_bbc_fvtx << " & " << float_bbc_fvtxN << " & " << float_fvtxN_fvtx << " \\\\ " << endl;
+  cout << reso_BBCS << " " << ereso_BBCS << endl;
+
+  // cout << energy << " GeV & " << reso_BBCS << " & " << reso_FVTXS << " & " << float_BBCS_FVTXS << " & " << float_BBCS_CNT << " & " << float_CNT_FVTXS << " \\\\ " << endl;
+  // cout << energy << " GeV & " << reso_BBCS_fn << " & " << reso_FVTXS_fn << " & " << reso_FVTXN_fn << " & " << float_BBCS_FVTXS << " & " << float_BBCS_FVTXN << " & " << float_FVTXN_FVTXS << " \\\\ " << endl;
 
 }
 
