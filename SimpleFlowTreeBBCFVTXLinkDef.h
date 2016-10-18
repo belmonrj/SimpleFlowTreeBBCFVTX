8ed94a7a1d8f59a11cd6c38cf286d56107936953
Ron Belmont <belmonrj@gmail.com>
2016-10-18 12:23:20 -0400

    Name change in progress, updated LinkDef and Makefile.am

---
+++ SimpleFlowTreeBBCFVTXLinkDef.h
@@ -0,0 +1,5 @@
+#ifdef __CINT__
+
+#pragma link C++ class SimpleFlowTreeBBCFVTX-!;
+
+#endif /* __CINT__ */
--- VTX_event_planeLinkDef.h
+++
@@ -1,5 +0,0 @@
-#ifdef __CINT__
-
-#pragma link C++ class VTX_event_plane_reco-!;
-
-#endif /* __CINT__ */
