910f87fd80f0c459edc39016d0246d31ebd2f2a4
Ron Belmont <belmonrj@gmail.com>
2016-11-06 18:45:52 -0500

    Added function to calculate corrected and a few quick TGraphs

--- wrk/MakePlots/temp_new_4pc.C
+++ wrk/MakePlots/temp_new_4pc.C
@@ -1,6 +1,6 @@
 void takehistograms(TProfile*,TProfile*,TProfile*,TProfile*,TProfile*,TProfile*,TProfile*,TProfile*);
 
-void calc_corr_four(double,double,double,double,double,double,double,double);
+double calc_corr_four(double,double,double,double,double,double,double,double);
 
 void temp_new_4pc()
 {
@@ -67,7 +67,34 @@ void takehistograms
       // --- some corrections...
       uncorr_c4[i] = four[i] - 2*two[i]*two[i]; // not very useful but have for completeness
       corr_c2[i] = two[i] - cos1[i]*cos1[i] - sin1[i]*sin1[i]; // simple enough it doesn't need it's own function
-      corr_c4[i] = calc_corr_four(); // come back to this asap
+      corr_c4[i] = calc_corr_four(four[i],two[i],cos1[i],sin1[i],cossum2[i],sinsum2[i],cos3[i],sin3[i]);
     }
 
+  TGraphErrors* tge_corr_c24 = new TGraphErrors(nbins,x,corr_c4,0,0);
+  tge_corr_c24->SetMarkerStyle(kOpenCircle);
+  tge_corr_c24->SetMarkerColor(kBlack);
+  tge_corr_c24->Draw("ap");
+  c1->Print("testcorr4pc.png");
+
+  TGraphErrors* tge_corr_c22 = new TGraphErrors(nbins,x,corr_c2,0,0);
+  tge_corr_c22->SetMarkerStyle(kOpenCircle);
+  tge_corr_c22->SetMarkerColor(kBlack);
+  tge_corr_c22->Draw("ap");
+  c1->Print("testcorr2pc.png");
+
+}
+
+double calc_corr_four(double four, double two, double cos1, double sin1, double cossum2, double sinsum2, double cos3, double sin3)
+{
+  double uncorr = four - 2*two*two;
+  double corr_term1 = 4*cos1*cos3;
+  double corr_term2 = 4*sin1*sin3;
+  double corr_term3 = cossum2*cossum2;
+  double corr_term4 = sinsum2*sinsum2;
+  double corr_term5 = 4*cossum2*(cos1*cos1 - sin1*sin1);
+  double corr_term6 = 8*sinsum2*sin1*cos1;
+  double corr_term7 = 8*two*(cos1*cos1 + sin1*sin1);
+  double corr_term8 = 6*(cos1*cos1 + sin1*sin1);
+  double result = uncorr - corr_term1 + corr_term2 - corr_term3 - corr_term4 + corr_term5 + corr_term6 + corr_term7 - corr_term8;
+  return result;
 }
