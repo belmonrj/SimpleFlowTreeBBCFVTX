7ba9b5ce6ec1757cadc0d302d40679baac41fee8
Ron Belmont <belmonrj@gmail.com>
2016-11-15 21:13:43 -0500

    Added a bunch of good stuff to delta

--- wrk/MakePlots/delta.C
+++ wrk/MakePlots/delta.C
@@ -1,6 +1,6 @@
 const float pi = 3.1415926535;
 
-TH1D* pleasefixme(TH1D*);
+TH1D* pleasefixme3h(TH1D*);
 
 
 void delta()
@@ -16,73 +16,106 @@ void delta()
   TH1D* th1d2 = (TH1D*)file->Get("fvtxs2_v3_both_phipsi");
   TH1D* th1d3 = (TH1D*)file->Get("fvtxs3_v3_both_phipsi");
 
-  TH1D* th1dA_fixed = pleasefixme(th1dA);
+  TH1D* th1dA_fixed = pleasefixme3h(th1dA);
   th1dA_fixed->Draw();
   double max = th1dA_fixed->GetBinContent(th1dA_fixed->GetMaximumBin());
   th1dA_fixed->SetMaximum(max*1.002);
   th1dA_fixed->SetMinimum(max*0.995);
   th1dA_fixed->GetXaxis()->SetRangeUser(-1.2,1.2);
   th1dA_fixed->GetXaxis()->SetTitle("#phi-#Psi_{3}");
-  TF1* fun = new TF1("fun","[0]+[1]*TMath::Cos(3*x-[2])",-1.2,1.2);
+  TF1* fun = new TF1("fun","[0]+[1]*TMath::Cos(3*x-[2])",-2,2);
   TLatex tex(0.5,0.8,Form("offset %.2e",fun->GetParameter(2)));
   tex.SetNDC();
   fun->SetParLimits(2,-0.9,0.9);
-  th1dA_fixed->Fit(fun,"R");
+  th1dA_fixed->Fit(fun,"","",-1.1,1.1);
   tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
   c1->Print("phipsi3_th1dA.png");
   c1->Print("phipsi3_th1dA.pdf");
 
-  TH1D* th1d0_fixed = pleasefixme(th1d0);
+  TH1D* th1d0_fixed = pleasefixme3h(th1d0);
   th1d0_fixed->Draw();
   max = th1d0_fixed->GetBinContent(th1d0_fixed->GetMaximumBin());
   th1d0_fixed->SetMaximum(max*1.002);
   th1d0_fixed->SetMinimum(max*0.995);
   th1d0_fixed->GetXaxis()->SetRangeUser(-1.2,1.2);
   th1d0_fixed->GetXaxis()->SetTitle("#phi-#Psi_{3}");
-  th1d0_fixed->Fit(fun,"R");
+  th1d0_fixed->Fit(fun,"","",-1.1,1.1);
   tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
   c1->Print("phipsi3_th1d0.png");
   c1->Print("phipsi3_th1d0.pdf");
 
-  TH1D* th1d1_fixed = pleasefixme(th1d1);
+  TH1D* th1d1_fixed = pleasefixme3h(th1d1);
   th1d1_fixed->Draw();
   max = th1d1_fixed->GetBinContent(th1d1_fixed->GetMaximumBin());
   th1d1_fixed->SetMaximum(max*1.002);
   th1d1_fixed->SetMinimum(max*0.995);
   th1d1_fixed->GetXaxis()->SetRangeUser(-1.2,1.2);
   th1d1_fixed->GetXaxis()->SetTitle("#phi-#Psi_{3}");
-  th1d1_fixed->Fit(fun,"R");
+  th1d1_fixed->Fit(fun,"","",-1.1,1.1);
   tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
   c1->Print("phipsi3_th1d1.png");
   c1->Print("phipsi3_th1d1.pdf");
 
-  TH1D* th1d2_fixed = pleasefixme(th1d2);
+  TH1D* th1d2_fixed = pleasefixme3h(th1d2);
   th1d2_fixed->Draw();
   max = th1d2_fixed->GetBinContent(th1d2_fixed->GetMaximumBin());
   th1d2_fixed->SetMaximum(max*1.002);
   th1d2_fixed->SetMinimum(max*0.995);
   th1d2_fixed->GetXaxis()->SetRangeUser(-1.2,1.2);
   th1d2_fixed->GetXaxis()->SetTitle("#phi-#Psi_{3}");
-  th1d2_fixed->Fit(fun,"R");
+  th1d2_fixed->Fit(fun,"","",-1.1,1.1);
   tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
   c1->Print("phipsi3_th1d2.png");
   c1->Print("phipsi3_th1d2.pdf");
 
-  TH1D* th1d3_fixed = pleasefixme(th1d3);
+  TH1D* th1d3_fixed = pleasefixme3h(th1d3);
   th1d3_fixed->Draw();
   max = th1d3_fixed->GetBinContent(th1d3_fixed->GetMaximumBin());
   th1d3_fixed->SetMaximum(max*1.002);
   th1d3_fixed->SetMinimum(max*0.995);
   th1d3_fixed->GetXaxis()->SetRangeUser(-1.2,1.2);
   th1d3_fixed->GetXaxis()->SetTitle("#phi-#Psi_{3}");
-  th1d3_fixed->Fit(fun,"R");
+  th1d3_fixed->Fit(fun,"","",-1.1,1.1);
   tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
   c1->Print("phipsi3_th1d3.png");
   c1->Print("phipsi3_th1d3.pdf");
 
+  // ---
+
+  TH1D* th1d_B = (TH1D*)file->Get("th1d_os_dreso2_BBC_CNT");
+  TH1D* th1d_S = (TH1D*)file->Get("th1d_os_dreso2_CNT_FVTX");
+
+  TF1* fun2 = new TF1("fun2","[0]+[1]*TMath::Cos(2*x-[2])",-2,2);
+
+  TH1D* th1d_B_fixed = pleasefixme2h(th1d_B);
+  TH1D* th1d_S_fixed = pleasefixme2h(th1d_S);
+
+  th1d_B_fixed->Draw();
+  max = th1d_B_fixed->GetBinContent(th1d_B_fixed->GetMaximumBin());
+  th1d_B_fixed->SetMaximum(max*1.002);
+  th1d_B_fixed->SetMinimum(max*0.95);
+  th1d_B_fixed->GetXaxis()->SetRangeUser(-1.7,1.7);
+  th1d_B_fixed->GetXaxis()->SetTitle("#phi-#Psi_{2}");
+  th1d_B_fixed->Fit(fun2,"","",-1.6,1.6);
+  tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
+  c1->Print("phipsi2_th1d_B.png");
+  c1->Print("phipsi2_th1d_B.pdf");
+
+  th1d_S_fixed->Draw();
+  max = th1d_S_fixed->GetBinContent(th1d_S_fixed->GetMaximumBin());
+  th1d_S_fixed->SetMaximum(max*1.002);
+  th1d_S_fixed->SetMinimum(max*0.92);
+  th1d_S_fixed->GetXaxis()->SetRangeUser(-1.7,1.7);
+  th1d_S_fixed->GetXaxis()->SetTitle("#phi-#Psi_{2}");
+  th1d_S_fixed->Fit(fun2,"","",-1.6,1.6);
+  tex.DrawLatex(0.65,0.82,Form("offset %.2e",fun->GetParameter(2)));
+  c1->Print("phipsi2_th1d_S.png");
+  c1->Print("phipsi2_th1d_S.pdf");
+
+
 }
 
-TH1D* pleasefixme(TH1D* in)
+TH1D* pleasefixme3h(TH1D* in)
 {
   TH1D* clone = (TH1D*)in->Clone();
   int nbins = clone->GetNbinsX();
@@ -115,6 +148,45 @@ TH1D* pleasefixme(TH1D* in)
 	{
 	  clone2->SetBinContent(i+1,0);
 	  clone2->Fill(phi-(2*pi/3),amount);
+	}
+    }
+  return clone2;
+}
+
+
+TH1D* pleasefixme2h(TH1D* in)
+{
+  TH1D* clone = (TH1D*)in->Clone();
+  int nbins = clone->GetNbinsX();
+  for ( int i = 0; i < nbins; ++i )
+    {
+      float phi = clone->GetBinCenter(i+1);
+      float amount = clone->GetBinContent(i+1);
+      if ( phi < -pi )
+	{
+	  clone->SetBinContent(i+1,0);
+	  clone->Fill(phi+2*pi,amount);
+	}
+      if ( phi > pi )
+	{
+	  clone->SetBinContent(i+1,0);
+	  clone->Fill(phi-2*pi,amount);
+	}
+    }
+  TH1D* clone2 = (TH1D*)clone->Clone();
+  for ( int i = 0; i < nbins; ++i )
+    {
+      float phi = clone2->GetBinCenter(i+1);
+      float amount = clone2->GetBinContent(i+1);
+      if ( phi < -pi/2 )
+	{
+	  clone2->SetBinContent(i+1,0);
+	  clone2->Fill(phi+pi,amount);
+	}
+      if ( phi > pi/2 )
+	{
+	  clone2->SetBinContent(i+1,0);
+	  clone2->Fill(phi-pi,amount);
 	}
     }
   return clone2;
