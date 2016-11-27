dcbc224c6265cd1ab045f5b3c8574c72b489528b
Ron Belmont <belmonrj@gmail.com>
2016-11-27 11:23:46 -0500

    More cosmetic changes

--- wrk/cumulants.cpp
+++ wrk/cumulants.cpp
@@ -130,82 +130,6 @@ void documulants(int runNumber)
       doweights = false;
     }
 
-  // TH2D* th2d_fvtxs_phi_weight[5];
-  // TH2D* th2d_fvtxn_phi_weight[5];
-  // if ( doweights )
-  //   {
-  //     TString histname0 = Form("th2d_fvtxs_clus_phi_weight" );
-  //     TString histname1 = Form("th2d_fvtxs0_clus_phi_weight");
-  //     TString histname2 = Form("th2d_fvtxs1_clus_phi_weight");
-  //     TString histname3 = Form("th2d_fvtxs2_clus_phi_weight");
-  //     TString histname4 = Form("th2d_fvtxs3_clus_phi_weight");
-  //     th2d_fvtxs_phi_weight[0] = (TH2D*)phi_weight_file->Get(histname0);
-  //     th2d_fvtxs_phi_weight[1] = (TH2D*)phi_weight_file->Get(histname1);
-  //     th2d_fvtxs_phi_weight[2] = (TH2D*)phi_weight_file->Get(histname2);
-  //     th2d_fvtxs_phi_weight[3] = (TH2D*)phi_weight_file->Get(histname3);
-  //     th2d_fvtxs_phi_weight[4] = (TH2D*)phi_weight_file->Get(histname4);
-  //     bool have_all_weight_histos =
-  //       th2d_fvtxs_phi_weight[0] &&
-  //       th2d_fvtxs_phi_weight[1] &&
-  //       th2d_fvtxs_phi_weight[2] &&
-  //       th2d_fvtxs_phi_weight[3] &&
-  //       th2d_fvtxs_phi_weight[4];
-  //     if ( verbosity > 0 || ( !have_all_weight_histos ) )
-  //       {
-  //         cout << "memory address of th2d_fvtxs_phi_weight[0] is " << th2d_fvtxs_phi_weight[0] << endl;
-  //         cout << "memory address of th2d_fvtxs_phi_weight[1] is " << th2d_fvtxs_phi_weight[1] << endl;
-  //         cout << "memory address of th2d_fvtxs_phi_weight[2] is " << th2d_fvtxs_phi_weight[2] << endl;
-  //         cout << "memory address of th2d_fvtxs_phi_weight[3] is " << th2d_fvtxs_phi_weight[3] << endl;
-  //         cout << "memory address of th2d_fvtxs_phi_weight[4] is " << th2d_fvtxs_phi_weight[4] << endl;
-  //         cout << "name of th2d_fvtxs_phi_weight[0] is " << histname0.Data() << endl;
-  //         cout << "name of th2d_fvtxs_phi_weight[1] is " << histname1.Data() << endl;
-  //         cout << "name of th2d_fvtxs_phi_weight[2] is " << histname2.Data() << endl;
-  //         cout << "name of th2d_fvtxs_phi_weight[3] is " << histname3.Data() << endl;
-  //         cout << "name of th2d_fvtxs_phi_weight[4] is " << histname4.Data() << endl;
-  //         cout << "The phi weight file read in was " << phiweightfile_name.Data() << endl;
-  //       } // verbosity check
-  //     if ( !have_all_weight_histos )
-  //       {
-  //         cout << "WARNING: not all weight histograms present" << endl;
-  //         doweights = false;
-  //       } // check on existence of histos
-  //         // --- now get the weights for FVTX North
-  //     histname0 = Form("th2d_fvtxn_clus_phi_weight" );
-  //     histname1 = Form("th2d_fvtxn0_clus_phi_weight");
-  //     histname2 = Form("th2d_fvtxn1_clus_phi_weight");
-  //     histname3 = Form("th2d_fvtxn2_clus_phi_weight");
-  //     histname4 = Form("th2d_fvtxn3_clus_phi_weight");
-  //     th2d_fvtxn_phi_weight[0] = (TH2D*)phi_weight_file->Get(histname0);
-  //     th2d_fvtxn_phi_weight[1] = (TH2D*)phi_weight_file->Get(histname1);
-  //     th2d_fvtxn_phi_weight[2] = (TH2D*)phi_weight_file->Get(histname2);
-  //     th2d_fvtxn_phi_weight[3] = (TH2D*)phi_weight_file->Get(histname3);
-  //     th2d_fvtxn_phi_weight[4] = (TH2D*)phi_weight_file->Get(histname4);
-  //     have_all_weight_histos =
-  //       th2d_fvtxn_phi_weight[0] &&
-  //       th2d_fvtxn_phi_weight[1] &&
-  //       th2d_fvtxn_phi_weight[2] &&
-  //       th2d_fvtxn_phi_weight[3] &&
-  //       th2d_fvtxn_phi_weight[4];
-  //     if ( verbosity > 0 || ( !have_all_weight_histos ) )
-  //       {
-  //         cout << "memory address of th2d_fvtxn_phi_weight[0] is " << th2d_fvtxn_phi_weight[0] << endl;
-  //         cout << "memory address of th2d_fvtxn_phi_weight[1] is " << th2d_fvtxn_phi_weight[1] << endl;
-  //         cout << "memory address of th2d_fvtxn_phi_weight[2] is " << th2d_fvtxn_phi_weight[2] << endl;
-  //         cout << "memory address of th2d_fvtxn_phi_weight[3] is " << th2d_fvtxn_phi_weight[3] << endl;
-  //         cout << "memory address of th2d_fvtxn_phi_weight[4] is " << th2d_fvtxn_phi_weight[4] << endl;
-  //         cout << "name of th2d_fvtxn_phi_weight[0] is " << histname0.Data() << endl;
-  //         cout << "name of th2d_fvtxn_phi_weight[1] is " << histname1.Data() << endl;
-  //         cout << "name of th2d_fvtxn_phi_weight[2] is " << histname2.Data() << endl;
-  //         cout << "name of th2d_fvtxn_phi_weight[3] is " << histname3.Data() << endl;
-  //         cout << "name of th2d_fvtxn_phi_weight[4] is " << histname4.Data() << endl;
-  //         cout << "The phi weight file read in was " << phiweightfile_name.Data() << endl;
-  //       } // verbosity check
-  //     if ( !have_all_weight_histos )
-  //       {
-  //         cout << "WARNING: not all weight histograms present" << endl;
-  //         doweights = false;
-  //       } // check on existence of histos
-  //   } // check on doweights
 
   TH1D* th1d_fvtxs_phi_weight[5];
   TH1D* th1d_fvtxn_phi_weight[5];
@@ -762,15 +686,6 @@ void documulants(int runNumber)
 	  float fvtx_weight = 1.0;
 	  if ( doweights )
 	    {
-	      // if ( !th2d_fvtxs_phi_weight[fvtx_layer+1] )
-	      // 	{
-	      // 	  cout << "WARNING!!!  Problem with weight histograms in cluster loop..." << endl;
-	      // 	  continue;
-	      // 	}
-	      // int phi_bin = th2d_fvtxs_phi_weight[fvtx_layer+1]->GetYaxis()->FindBin(phi); // COME BACK HERE AND HAVE A LOOK
-	      // int zvtx_bin = th2d_fvtxs_phi_weight[fvtx_layer+1]->GetXaxis()->FindBin(ZVTX); // COME BACK HERE AND HAVE A LOOK
-	      // fvtx_weight = th2d_fvtxs_phi_weight[fvtx_layer+1]->GetBinContent(zvtx_bin,phi_bin);
-	      // ---
 	      if ( !th1d_fvtxs_phi_weight[fvtx_layer+1] )
 		{
 		  cout << "WARNING!!!  Problem with weight histograms in cluster loop..." << endl;
@@ -843,15 +758,6 @@ void documulants(int runNumber)
 	      // --- determine the weights for north clusters ---
 	  if ( doweights )
 	    {
-	      // if ( !th2d_fvtxn_phi_weight[fvtx_layer+1] )
-	      // 	{
-	      // 	  cout << "WARNING!!!  Problem with weight histograms in cluster loop..." << endl;
-	      // 	  continue;
-	      // 	}
-	      // int phi_bin = th2d_fvtxn_phi_weight[fvtx_layer+1]->GetYaxis()->FindBin(phi); // COME BACK HERE AND HAVE A LOOK
-	      // int zvtx_bin = th2d_fvtxn_phi_weight[fvtx_layer+1]->GetXaxis()->FindBin(ZVTX); // COME BACK HERE AND HAVE A LOOK
-	      // fvtx_weight = th2d_fvtxn_phi_weight[fvtx_layer+1]->GetBinContent(zvtx_bin,phi_bin);
-	      // ---
 	      if ( !th1d_fvtxn_phi_weight[fvtx_layer+1] )
 		{
 		  cout << "WARNING!!!  Problem with weight histograms in cluster loop..." << endl;
