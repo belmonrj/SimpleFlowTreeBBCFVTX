c8d73b9eaad12a579c24819a64f50c4af0d67870
Ron Belmont <belmonrj@gmail.com>
2017-03-29 16:07:23 -0400

    Added cumulant code for 3HeAu

--- wrk/ccompile.sh
+++ wrk/ccompile.sh
@@ -2,4 +2,5 @@
 
 g++ -o cumulants.exe cumulants.cpp -Wall -m32 -lstdc++ `root-config --libs` -I$ROOTSYS/include
 g++ -o cumulants_pAu.exe cumulants_pAu.cpp -Wall -m32 -lstdc++ `root-config --libs` -I$ROOTSYS/include
+g++ -o cumulants_3HeAu.exe cumulants_3HeAu.cpp -Wall -m32 -lstdc++ `root-config --libs` -I$ROOTSYS/include
 
