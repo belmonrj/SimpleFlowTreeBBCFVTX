fc74bc1328705c0991ad40b7777a5b1d8f53d9b1
Ron Belmont <belmonrj@gmail.com>
2017-05-11 17:16:54 -0400

    Major update to thing1 with selectivity for which combinations

--- wrk/CumulantStuff/thing1.C
+++ wrk/CumulantStuff/thing1.C
@@ -1,14 +1,61 @@
-void dothething(int&);
+
+const int number_of_tests = 6; // update as needed
+const int xhash = 0;
+const int xdesc = 1;
+TString hash_description[2][number_of_tests];
+
+void dothething(int, int, int, int, int);
 
 void thing1()
 {
 
-  dothething(200);
+  hash_description[0][0] = "aea49a4";  hash_description[1][0] = "Baseline"; // earlier baseline, different from others, same as ce076aa
+  hash_description[0][1] = "b917033";  hash_description[1][1] = "FtD  2dR"; // floats changed to doubles and uses 2d recentering
+  hash_description[0][2] = "75765b7";  hash_description[1][2] = "FtD only"; // floats changed to doubles but uses 1d recentering
+  hash_description[0][3] = "9fefb05";  hash_description[1][3] = "2dR only"; // uses 2d recentering but floats left as floats
+  hash_description[0][4] = "d52d914";  hash_description[1][4] = "Baseline"; // most recent baseline, agrees with modified ones
+  hash_description[0][5] = "ce076aa";  hash_description[1][5] = "Baseline"; // earlier baseline, different from others, same as aea49a4
+
+  dothething(200,0,1,4,5);
 
 }
 
-void dothething(int& name)
+void dothething(int name, int which1, int which2, int which3, int which4)
 {
+
+  TString ts_hash1 = hash_description[xhash][which1];
+  TString ts_hash2 = hash_description[xhash][which2];
+  TString ts_hash3 = hash_description[xhash][which3];
+  TString ts_hash4 = hash_description[xhash][which4];
+
+  TString ts_desc1 = hash_description[xdesc][which1];
+  TString ts_desc2 = hash_description[xdesc][which2];
+  TString ts_desc3 = hash_description[xdesc][which3];
+  TString ts_desc4 = hash_description[xdesc][which4];
+
+  // --- don't know why this doesn't work
+  // TString fname1 = Form("Cumulants_%s/cumulants_%d.root",ts_hash1,name);
+  // TString fname2 = Form("Cumulants_%s/cumulants_%d.root",ts_hash2,name);
+  // TString fname3 = Form("Cumulants_%s/cumulants_%d.root",ts_hash3,name);
+  // TString fname4 = Form("Cumulants_%s/cumulants_%d.root",ts_hash4,name);
+
+  // --- this seems to work fine
+  TString fname1 = "Cumulants_"; fname1 += ts_hash1; fname1 += "/cumulants_"; fname1 += name; fname1 += ".root";
+  TString fname2 = "Cumulants_"; fname2 += ts_hash2; fname2 += "/cumulants_"; fname2 += name; fname2 += ".root";
+  TString fname3 = "Cumulants_"; fname3 += ts_hash3; fname3 += "/cumulants_"; fname3 += name; fname3 += ".root";
+  TString fname4 = "Cumulants_"; fname4 += ts_hash4; fname4 += "/cumulants_"; fname4 += name; fname4 += ".root";
+
+  TString leghead1 = ts_desc1; leghead1 += " "; leghead1 += ts_hash1;
+  TString leghead2 = ts_desc2; leghead2 += " "; leghead2 += ts_hash2;
+  TString leghead3 = ts_desc3; leghead3 += " "; leghead3 += ts_hash3;
+  TString leghead4 = ts_desc4; leghead4 += " "; leghead4 += ts_hash4;
+
+  cout << ts_hash1 << " " << ts_desc1 << " " << fname1 << " " << leghead1 << endl;
+  cout << ts_hash2 << " " << ts_desc2 << " " << fname2 << " " << leghead2 << endl;
+  cout << ts_hash3 << " " << ts_desc3 << " " << fname3 << " " << leghead3 << endl;
+  cout << ts_hash4 << " " << ts_desc4 << " " << fname4 << " " << leghead4 << endl;
+
+
 
   TCanvas* c1 = new TCanvas();
   c1->SetMargin(0.15,0.05,0.13,0.08); // LRBT
@@ -18,15 +65,10 @@ void dothething(int& name)
   double ymin = 0.0;
   double ymax = 50.0;
 
-  // this version of "base" is rather different, working on pinning it down...
-  //TFile* file1 = TFile::Open(Form("../output/Cumulants_20170502-1159_gittagu_aea49a4_BeforeRecentChanges/cumulants_%d.root",name));
-  // this version of "base" agrees with the other three
-  //TFile* file1 = TFile::Open(Form("../output/Cumulants_20170511-0046_gittagu_d52d914_BeforeRecentChanges/cumulants_%d.root",name));
-  // this version of "base" looks like the early one...
-  TFile* file1 = TFile::Open(Form("../output/Cumulants_20170511-1218_gittagu_ce076aa_BeforeRecentChanges/cumulants_%d.root",name));
-  TFile* file2 = TFile::Open(Form("../output/Cumulants_20170510-1238_gittagu_b917033_FloatToDoubleAnd2dRecentering/cumulants_%d.root",name));
-  TFile* file3 = TFile::Open(Form("../output/Cumulants_20170510-1509_gittagu_75765b7_FloatToDoubleOnly/cumulants_%d.root",name));
-  TFile* file4 = TFile::Open(Form("../output/Cumulants_20170510-2143_gittagm_9fefb05_2dRecenteringOnly/cumulants_%d.root",name));
+  TFile* file1 = TFile::Open(fname1);
+  TFile* file2 = TFile::Open(fname2);
+  TFile* file3 = TFile::Open(fname3);
+  TFile* file4 = TFile::Open(fname4);
 
   // --- c22
 
@@ -66,10 +108,10 @@ void dothething(int& name)
   h3_os_c22->Draw("same ex0p");
   h4_os_c22->Draw("same ex0p");
   TLegend* leg_os_c22 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_os_c22->AddEntry(h1_os_c22,"Old","p");
-  leg_os_c22->AddEntry(h2_os_c22,"Double and 2dR","p");
-  leg_os_c22->AddEntry(h3_os_c22,"Double","p");
-  leg_os_c22->AddEntry(h4_os_c22,"2dR","p");
+  leg_os_c22->AddEntry(h1_os_c22,leghead1,"p");
+  leg_os_c22->AddEntry(h2_os_c22,leghead2,"p");
+  leg_os_c22->AddEntry(h3_os_c22,leghead3,"p");
+  leg_os_c22->AddEntry(h4_os_c22,leghead4,"p");
   leg_os_c22->SetTextSize(0.055);
   leg_os_c22->Draw();
   c1->Print(Form("ComparisonFigs/FourWayComparison_os_c22_%d.png",name));
@@ -113,10 +155,10 @@ void dothething(int& name)
   h3_os_c24->Draw("same ex0p");
   h4_os_c24->Draw("same ex0p");
   TLegend* leg_os_c24 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_os_c24->AddEntry(h1_os_c24,"Old","p");
-  leg_os_c24->AddEntry(h2_os_c24,"Double and 2dR","p");
-  leg_os_c24->AddEntry(h3_os_c24,"Double","p");
-  leg_os_c24->AddEntry(h4_os_c24,"2dR","p");
+  leg_os_c24->AddEntry(h1_os_c24,leghead1,"p");
+  leg_os_c24->AddEntry(h2_os_c24,leghead2,"p");
+  leg_os_c24->AddEntry(h3_os_c24,leghead3,"p");
+  leg_os_c24->AddEntry(h4_os_c24,leghead4,"p");
   leg_os_c24->SetTextSize(0.055);
   leg_os_c24->SetFillStyle(0);
   leg_os_c24->Draw();
@@ -161,10 +203,10 @@ void dothething(int& name)
   h3_os_c26->Draw("same ex0p");
   h4_os_c26->Draw("same ex0p");
   TLegend* leg_os_c26 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_os_c26->AddEntry(h1_os_c26,"Old","p");
-  leg_os_c26->AddEntry(h2_os_c26,"Double and 2dR","p");
-  leg_os_c26->AddEntry(h3_os_c26,"Double","p");
-  leg_os_c26->AddEntry(h4_os_c26,"2dR","p");
+  leg_os_c26->AddEntry(h1_os_c26,leghead1,"p");
+  leg_os_c26->AddEntry(h2_os_c26,leghead2,"p");
+  leg_os_c26->AddEntry(h3_os_c26,leghead3,"p");
+  leg_os_c26->AddEntry(h4_os_c26,leghead4,"p");
   leg_os_c26->SetTextSize(0.055);
   leg_os_c26->SetFillStyle(0);
   leg_os_c26->Draw();
@@ -209,10 +251,10 @@ void dothething(int& name)
   h3_ac_c22->Draw("same ex0p");
   h4_ac_c22->Draw("same ex0p");
   TLegend* leg_ac_c22 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_ac_c22->AddEntry(h1_ac_c22,"Old","p");
-  leg_ac_c22->AddEntry(h2_ac_c22,"Double and 2dR","p");
-  leg_ac_c22->AddEntry(h3_ac_c22,"Double","p");
-  leg_ac_c22->AddEntry(h4_ac_c22,"2dR","p");
+  leg_ac_c22->AddEntry(h1_ac_c22,leghead1,"p");
+  leg_ac_c22->AddEntry(h2_ac_c22,leghead2,"p");
+  leg_ac_c22->AddEntry(h3_ac_c22,leghead3,"p");
+  leg_ac_c22->AddEntry(h4_ac_c22,leghead4,"p");
   leg_ac_c22->SetTextSize(0.055);
   leg_ac_c22->Draw();
   c1->Print(Form("ComparisonFigs/FourWayComparison_ac_c22_%d.png",name));
@@ -256,10 +298,10 @@ void dothething(int& name)
   h3_ac_c24->Draw("same ex0p");
   h4_ac_c24->Draw("same ex0p");
   TLegend* leg_ac_c24 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_ac_c24->AddEntry(h1_ac_c24,"Old","p");
-  leg_ac_c24->AddEntry(h2_ac_c24,"Double and 2dR","p");
-  leg_ac_c24->AddEntry(h3_ac_c24,"Double","p");
-  leg_ac_c24->AddEntry(h4_ac_c24,"2dR","p");
+  leg_ac_c24->AddEntry(h1_ac_c24,leghead1,"p");
+  leg_ac_c24->AddEntry(h2_ac_c24,leghead2,"p");
+  leg_ac_c24->AddEntry(h3_ac_c24,leghead3,"p");
+  leg_ac_c24->AddEntry(h4_ac_c24,leghead4,"p");
   leg_ac_c24->SetTextSize(0.055);
   leg_ac_c24->SetFillStyle(0);
   leg_ac_c24->Draw();
@@ -304,10 +346,10 @@ void dothething(int& name)
   h3_ac_c26->Draw("same ex0p");
   h4_ac_c26->Draw("same ex0p");
   TLegend* leg_ac_c26 = new TLegend(0.58,0.68,0.88,0.88);
-  leg_ac_c26->AddEntry(h1_ac_c26,"Old","p");
-  leg_ac_c26->AddEntry(h2_ac_c26,"Double and 2dR","p");
-  leg_ac_c26->AddEntry(h3_ac_c26,"Double","p");
-  leg_ac_c26->AddEntry(h4_ac_c26,"2dR","p");
+  leg_ac_c26->AddEntry(h1_ac_c26,leghead1,"p");
+  leg_ac_c26->AddEntry(h2_ac_c26,leghead2,"p");
+  leg_ac_c26->AddEntry(h3_ac_c26,leghead3,"p");
+  leg_ac_c26->AddEntry(h4_ac_c26,leghead4,"p");
   leg_ac_c26->SetTextSize(0.055);
   leg_ac_c26->SetFillStyle(0);
   leg_ac_c26->Draw();
